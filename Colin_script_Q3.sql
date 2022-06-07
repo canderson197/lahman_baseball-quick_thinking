9ecbebec2d54be5793713e5c949e4130670a390c
canderson197 <colin.anderson197@gmail.com>
2022-06-07 13:08:37 -0500

    question 6, not yet reviewed

--- Colin_script_Q3.sql
+++ Colin_script_Q3.sql
@@ -43,4 +43,9 @@ SELECT DISTINCT playerid,
        SUM(salary) OVER(PARTITION BY playerid) as major_league_earnings
 FROM Vandy_alums LEFT JOIN people USING(playerid)
                  LEFT JOIN salaries USING(playerid) 
-                
+                 
+/*6. Find the player who had the most success stealing bases in 2016, where __success__ is measured 
+as the percentage of stolen base attempts which are successful. (A stolen base attempt results either in a stolen base 
+or being caught stealing.) Consider only players who attempted _at least_ 20 stolen bases.
+*/
+
